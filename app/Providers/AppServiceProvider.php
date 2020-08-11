2f8310197f53f50a6534be70afcee07d4f8d4412
MeowKim <ariesmilk51@gmail.com>
2020-08-11 10:10:20 +0900

    Share all routes via view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Get all the web routes
+        View::composer('*', function ($view) {
+            $routes = collect(Route::getRoutes())
+                ->reject(function ($route) {
+                    return preg_match("/^\_ignition\//", $route->uri) || preg_match("/^api\//", $route->uri);
+                })
+                ->map(function ($route) {
+                    return array(
+                        'domain' => $route->domain(),
+                        'method' => implode('|', $route->methods()),
+                        'uri'    => $route->uri(),
+                        'name'   => $route->getName(),
+                        'action' => ltrim($route->getActionName(), '\\'),
+                        'middleware' => collect($route->gatherMiddleware())
+                            ->map(function ($middleware) {
+                                return $middleware instanceof Closure ? 'Closure' : $middleware;
+                            })->implode(','),
+                    );
+                });
+
+            $view->with('routes', $routes);
+        });
         //
     }
 }
