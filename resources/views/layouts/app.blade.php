7844f4ff5e5f26d0c080cf802f98a2422b539485
MeowKim <ariesmilk51@gmail.com>
2020-08-11 10:42:27 +0900

    Add navigations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,29 @@
 </head>
 <body>
 	<div id="app">
+		<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm mb-3">
+			<div class="container">
+				<a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<!-- Left Side Of Navbar -->
+					<ul class="navbar-nav mr-auto">
+						@foreach ($routes as $route)
+						<li class="nav-item">
+							<a class="nav-link" href="{{ $route['uri'] }}">{{ $route['name'] }}</a>
+						</li>
+						@endforeach
+					</ul>
+				</div>
+			</div>
+		</nav>
 		<div class="container">
 			@yield('content')
 		</div>
